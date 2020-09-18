cd80cdc9432d193bf273f96b6ee7c4104e3f4cf6
DaniMoro <daniele.morotti.99@gmail.com>
2020-09-18 19:25:28 +0200

    [EDITOR] Added QR code creation.

--- Editor/js/home.js
+++ Editor/js/home.js
@@ -20,10 +20,21 @@ export default {
             <div id="stories">
                 <span v-if="storiesList.length == 0">Nessuna storia presente</span>
                 <div id="stories-list" class="list-group" v-else>
-                    <button v-for="(story,index) in storiesList" :key="index" type="button" class="list-group-item list-group-item-action" 
-                    @click="changeActive(index)"> {{story}} <span id="icon-group"><i class="fas fa-edit" @click="editStory(index)"></i>&nbsp;&nbsp;<i tabindex="0" class="fas fa-file-upload"></i>&nbsp;&nbsp;
-                    <i tabindex="0" class="fas fa-qrcode"></i>&nbsp;&nbsp;<i tabindex="0" class="fas fa-trash-alt" @click="deleteStory(index)"></i></span></button>
+                    <button v-for="(story,index) in storiesList" :key="index" type="button" class="list-group-item list-group-item-action" @click="changeActive(index)">
+                        {{story}} 
+                        <span id="icon-group">
+                            <i class="fas fa-edit" @click="editStory(index)" ></i>&nbsp;&nbsp;
+                            <i tabindex="0" class="fas fa-file-upload"></i>&nbsp;&nbsp;
+                            <i tabindex="0" class="fas fa-qrcode" @click="createQRCode(index)"></i>&nbsp;&nbsp;
+                            <i tabindex="0" class="fas fa-trash-alt" @click="deleteStory(index)"></i>
+                        </span>
+                    </button>
                 </div>
+            </div>
+            <div id="qrcodeMenu">
+                <div id="qrcode"></div>
+                <p></p>
+                <button @click="hideMenu">CHIUDI FINESTRA</button>
             </div>
         </div> 
     `,
@@ -76,9 +87,30 @@ export default {
                 console.log('emit');
                 bus.$emit('story',this.storiesList[index])
             });
+        },
+        createQRCode(index){
+            let story = this.storiesList[index];
+            new QRCode('qrcode', {
+                text: "http://site192001.tw.cs.unibo.it/stories?story="+story,
+                width: 128,
+                height: 128,
+                colorDark : "#000000",
+                colorLight : "#ffffff",
+                correctLevel : QRCode.CorrectLevel.H
+            });
+            $('#qrcodeMenu').show();
+            $('#qrcode + p').html("Per salvare l'immagine : <code>1)Click tasto destro del mouse sul QR code. 2)Salva con nome</code>");
+            /*Magari serviranno per cose future quindi ho lasciato qui queste 2 funzioni
+            qrcode.clear(); // clear the code.
+            qrcode.makeCode("http://naver.com"); // make another code.
+            */
+        },
+        hideMenu(){
+            $('#qrcodeMenu').hide();
+            //delete the current qr code
+            $('#qrcode').html("");
         }
     },
-  
     mounted() {        
         $.get( "/titles", (res) => {
             for(let i=0; i < res.length;i++){
